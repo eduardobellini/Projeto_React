89ad50b1d319cade17a999234e757f2eae8d62a3
eduardobellini-source <2024111790052@iesp.edu.br>
2025-11-10 20:55:00 -0300

    logica do calendario adicionada

--- vite-projeto/vite/src/components/Calendario/Calendario.tsx
+++ vite-projeto/vite/src/components/Calendario/Calendario.tsx
@@ -1,9 +1,24 @@
 import React from 'react'
+import './Calendario.css'
 
-function Calendario() {
-  return (
-    <><div>oi</div></>
-  )
+
+// Logica do calendario 
+const WEEK_DAYS = ['S', 'M', 'T', 'W', 'T', 'F', 'S'];
+const MONTHS = [
+  'Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio', 'Junho',
+  'Julho', 'Agosto', 'Setembro', 'Outubro', 'Novembro', 'Dezembro'
+];
+
+function getDaysArray(year: number, month: number) {
+  const firstDayOfMonth = new Date(year, month, 1).getDay();
+  const daysInMonth = new Date(year, month + 1, 0).getDate();
+  const days: (number | null)[] = [];
+
+  for (let i = 0; i < firstDayOfMonth; i++) {
+    days.push(null);
+  }
+  for (let i = 1; i <= daysInMonth; i++) {
+    days.push(i);
+  }
+  return days;
 }
-
-export default Calendario
