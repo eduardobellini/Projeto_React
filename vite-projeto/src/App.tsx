5a9396f3180a2521d57276f2bd5923934e13fd42
eduardobellini <eduardombellini@gmail.com>
2025-11-11 08:14:15 -0300

    refazendo o packeg json, pois deu bug na hora de rodar o npm run dev

--- vite-projeto/src/App.tsx
+++ vite-projeto/src/App.tsx
@@ -1,16 +1,16 @@
 
 import './App.css'
-import { Calendario } from './components/Calendario/Calendario'
-import { MeusHabitos } from './components/MeusHabitos/MeusHabitos';
 import Calendario from './components/Calendario/Calendario'
+import MeusHabitos from './components/MeusHabitos/MeusHabitos'
+
 
 
 function App() {
 
   return (
     <>
-      <Calendario />
-      <MeusHabitos />
+      <Calendario/>
+      <MeusHabitos/>
     </>
   )
 }
