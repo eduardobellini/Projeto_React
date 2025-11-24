a46a7a363565c1c5bae80092993f58cd5c6ac3fd
eduardobellini-source <2024111790052@iesp.edu.br>
2025-11-24 14:39:13 -0300

    atualizando o componente principal para usar AppRoutes em vez de App

--- vite-projeto/src/main.tsx
+++ vite-projeto/src/main.tsx
@@ -1,10 +1,12 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.tsx'
+import AppRoutes from './routes'
+// Importação global de estilos do Ant Design (necessária se usar componentes do antd)
+import 'antd/dist/reset.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <AppRoutes />
   </StrictMode>,
 )
