f12e01abcdfe70cdbc4b8bb41b16cef3fd038c09
eduardobellini-source <2024111790052@iesp.edu.br>
2025-11-24 14:39:34 -0300

    merge das rotas e pages

---
+++ vite-projeto/src/routes/index.tsx
@@ -0,0 +1,19 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import Home from "../pages/Home/home";
+import Contato from "../pages/Contato/Contato";
+import Sobre from "../pages/Sobre/Sobre";
+
+export default function AppRoutes() {
+    return (
+        <BrowserRouter>
+        <Routes>
+            
+            <Route path="/" element={<Home />}/>
+            <Route path="/sobre" element={<Sobre />}/>
+            <Route path="/contato" element={<Contato />}/>
+  
+         </Routes>
+       </BrowserRouter>
+    );
+}
