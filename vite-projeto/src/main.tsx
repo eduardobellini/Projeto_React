c7c89a93932971f70fc7659d2fc54f819d7c8ce3
eduardobellini-source <2024111790052@iesp.edu.br>
2025-11-24 14:41:57 -0300

    correção

--- vite-projeto/src/main.tsx
+++ vite-projeto/src/main.tsx
@@ -2,7 +2,6 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import AppRoutes from './routes'
-// Importação global de estilos do Ant Design (necessária se usar componentes do antd)
 import 'antd/dist/reset.css'
 
 createRoot(document.getElementById('root')!).render(
